d3bdbe74166a91a69e2a1db91e7862c5acbfe31d
Okinbird <okinyinka@yahoo.co.uk>
2022-09-22 16:25:08 -0700

    calculator f-spring

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,14 +4,14 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
         ops = {"+": add, "-": sub, "*": mul, "/": div}
         if sys.argv[2] not in list(ops.keys()):
-            print("Unknown operator. Available operators: +, -, * and /")
+            print(f"Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
 
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+        print(f"{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
