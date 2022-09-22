ebb8ed2bf14c393f797e43c0d12a9f42e9b9bc05
Okinbird <okinyinka@yahoo.co.uk>
2022-09-22 16:17:14 -0700

    hidden discovery print format

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    name = dir(hidden_4)
-    for name in name:
+    for name in dir(hidden_4):
         if name[:2] != "__":
             print("{:s}".format(name))
