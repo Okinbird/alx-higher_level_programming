e1970655e07268c75460da877e4e483eee7dc3bb
Okinbird <okinyinka@yahoo.co.uk>
2022-09-28 13:57:01 -0700

    indentation error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -19,7 +19,7 @@ def roman_to_int(roman_string):
             return (0)
 
         if (i != (len(roman_string) - 1) and roman_dict[roman_string[i]]
-            < roman_dict[roman_string[i + 1]]):
+                    < roman_dict[roman_string[i + 1]]):
             num += roman_dict[roman_string[i]] * -1
 
         else:
