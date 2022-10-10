18b2299b36249af4b60d46c13a9aaef4eb4f741e
Okinbird <okinyinka@yahoo.co.uk>
2022-10-10 14:57:41 -0700

    python bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
-        except (TypeError, ZeroDivisionError):
+        except:
             result = b + a
             break
     return (result)
