cf2bcbdd46a926626804cf1103fce739a14e71b7
Okinbird <okinyinka@yahoo.co.uk>
2022-10-10 14:29:47 -0700

    divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,4 +15,4 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             new_list.append(result)
-            return (new_list)
+    return (new_list)
