f6ce5800a0bba40d56a670de1152eece7430a3c2
Okinbird <okinyinka@yahoo.co.uk>
2022-10-11 10:27:50 -0700

    fixed indentation error

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -35,9 +35,9 @@ class Square:
         Returns:
             None
         """
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
