64b62fad648e7ab861ff6d067d23fdb6b123cd08
Okinbird <okinyinka@yahoo.co.uk>
2022-10-18 13:45:35 -0700

    exception

--- 0x08-python-more_classes/9-main.py
+++ 0x08-python-more_classes/9-main.py
@@ -4,3 +4,21 @@ Rectangle = __import__('9-rectangle').Rectangle
 my_square = Rectangle.square(5)
 print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
 print(my_square)
+print()
+mysquare = Rectangle.square(10)
+print("Width: {} / height: {}".format(mysquare.width, my_square.height))
+print()
+mysquare = Rectangle.square(2)
+print("{} / {}".format(mysquare.width, my_square.height))
+print()
+
+mysquare = Rectangle.square(-2)
+print("{} / {}".format(mysquare.width, mysquare.height))
+except Exception as e:
+    print("[{}] {}".format(e.class.name_, e))
+print()
+
+mysquare = Rectangle.square("12")
+print("{} / {}".format(mysquare.width, mysquare.height))
+except Exception as e:
+    print("[{}] {}".format(e.class.name_, e))
