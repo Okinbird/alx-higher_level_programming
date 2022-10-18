a0e61635e905cf3986e5f43152fc94c3439c9130
Okinbird <okinyinka@yahoo.co.uk>
2022-10-18 10:03:15 -0700

    class method: define square as rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -79,7 +79,7 @@ class Rectangle:
         Args:
             size (int): The width and height of the new Rectangle.
         """
-        return (cls(size, size))
+        return cls(size, size)
 
     def area(self):
         """Return the area of the Rectangle."""
