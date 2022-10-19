7ad7c4e53255768bef8ed022bcac4d49589f9ba4
Okinbird <okinyinka@yahoo.co.uk>
2022-10-19 09:52:24 -0700

    fix code

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-        type(self).number_of_instances += 1
+        Rectangle.number_of_instances += 1
         self.height = height
         self.width = width
 
