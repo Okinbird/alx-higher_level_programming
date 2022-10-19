e77ce367b9dbcfc7c41e17fed29c5f66dc41fec3
Okinbird <okinyinka@yahoo.co.uk>
2022-10-19 09:13:53 -0700

    task 8 to 34

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class LockedClass:
+    """Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'
+    """
+    __slots__ = ['first_name']
