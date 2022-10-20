97d71612b6b3927cf7a1ebb212f07142c36d2cb8
Okinbird <okinyinka@yahoo.co.uk>
2022-10-20 14:41:24 -0700

    python strings

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,28 @@
+#include <Python.h>
+
+/**
+ * print_python_string - function that prints Python strings
+ * @p: Python string
+ * Return: void
+ */
+void print_python_string(PyObject *p)
+{
+  ssize_t len;
+
+  printf("[.] string object info\n");
+
+  if (strcmp(p->ob_type->tp_name, "str") != 0)
+    {
+      printf("  [ERROR] Invalid String Object\n");
+      return;
+    }
+
+  if (PyUnicode_IS_COMPACT_ASCII(p))
+    printf("  type: compact ascii\n");
+  else
+    printf("  type: compact unicode object\n");
+
+  len = PyUnicode_GET_LENGTH(p);
+  printf("  length: %ld\n", len);
+  printf("  value: %s\n", PyUnicode_AsUTF8AndSize(p, &len));
+}
