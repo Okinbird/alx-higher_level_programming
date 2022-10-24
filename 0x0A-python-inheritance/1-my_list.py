136212b85a10deca71c2ba307d9c004c596be228
Okinbird <okinyinka@yahoo.co.uk>
2022-10-24 09:42:14 -0700

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines an inherited list class MyList
+"""
+
+
+class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """Print a list in sorted ascenfing order"""
+        print(sorted(self))
