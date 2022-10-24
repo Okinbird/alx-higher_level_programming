484ae6a087a5698bb964ba48547ec52ac5775b0c
Okinbird <okinyinka@yahoo.co.uk>
2022-10-24 12:52:30 -0700

    indentation error

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,8 +5,7 @@ Defines an inherited list class MyList
 
 
 class MyList(list):
-     """Implements sorted printing for the built-in list class"""
-     
-     def print_sorted(self):
-          """Print a list in sorted ascending order"""
-          print(sorted(self))
+    """Implements sorted printing for the built-in list class"""
+    def print_sorted(self):
+        """Print a list in sorted ascending order"""
+        print(sorted(self))
