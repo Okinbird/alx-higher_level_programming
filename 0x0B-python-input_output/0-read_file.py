cec47b0a4be59101561a912ec0d254443f11bbfc
Okinbird <okinyinka@yahoo.co.uk>
2022-10-26 04:35:04 -0700

    read fie

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, "r", encoding="utf-8") as text:
-        print(text.read(), end="")
+    print(text.read(), end="")
