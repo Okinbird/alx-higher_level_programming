4725d97117ef6cec66674a9e154535c2afd43b18
Okinbird <okinyinka@yahoo.co.uk>
2022-10-31 17:36:03 -0700

    update base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,23 @@
 import json
 import csv
 import turtle
+import tkinter as tk
+import sys
+import os
+if os.environ.get('DISPLAY', '') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
+
+from tkinter import ttk
+
+root = tk.Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+root.mainloop()
 
 
 class Base:
