1fb929c4f4d20fbd5e52c2639e679085f18a23d4
Okinbird <okinyinka@yahoo.co.uk>
2022-11-30 12:34:05 -0800

    create read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,6 +7,5 @@ CREATE USER
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
    ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+   TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
