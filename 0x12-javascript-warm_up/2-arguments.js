3fc25b263ef711bffbfeeb29f71f1ff54dbe18c8
Okinbird <okinyinka@yahoo.co.uk>
2022-12-19 05:46:11 -0800

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-if (process.argv.length <= 0) {
+if (process.argv.length === 2) {
   console.log('No argument');
 }
-if (process.argv.length === 1) {
+if (process.argv.length === 3) {
   console.log('Argument found');
 }
-if (process.argv.length > 1) {
+if (process.argv.length > 3) {
   console.log('Arguments found');
 }
--- 0x12-javascript-warm_up/README.md
+++ 0x12-javascript-warm_up/README.md
@@ -11,7 +11,7 @@ Write a script that prints “JavaScript is amazing”:
 
 
 
-#   3 languages
+#   1. 3 languages
 
 Write a script that prints 3 lines:
 
