e26e438a8024abf74116bc86a7e0a40417c1d705
Okinbird <okinyinka@yahoo.co.uk>
2022-12-20 10:10:37 -0800

    empty class rec

---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,8 @@
+##  0x13. JavaScript - Objects, Scopes and Closures
+
+
+#   0. Rectangle #0
+
+Write an empty class `Rectangle` that defines a rectangle:
+
+*   You must use the `class` notation for defining your class
---
+++ 0x13-javascript_objects_scopes_closures/test_files/0-main.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const Rectangle = require('/home/okinamos/alx-higher_level_programming/0x13-javascript_objects_scopes_closures/0-rectangle');
+
+const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.constructor);
