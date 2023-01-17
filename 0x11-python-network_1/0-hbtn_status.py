95143f72ffa5927b5853bcafea264c2b61df5bcd
Okinbird <okinyinka@yahoo.co.uk>
2023-01-17 10:00:45 -0800

    fix pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
-    as response:
-        html = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as ul:
+        html = ul.read()
         print('Body response:')
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
