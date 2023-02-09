3d03973d2ab719137252bf40576ba703c706fbd1
Okinbird <okinyinka@yahoo.co.uk>
2023-02-09 06:37:25 -0800

    Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(file, content, 'utf-8', error => {
+  if (error) console.log(error);
+});
