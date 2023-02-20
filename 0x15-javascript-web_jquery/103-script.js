a18db2372348007bb9453cc506b6bf6d0818eb04
Okinbird <okinyinka@yahoo.co.uk>
2023-02-20 14:20:46 -0800

    semistandard test files

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,22 +1,20 @@
-$(document).ready(function () {
-  function translate () {
-    $('DIV#hello').empty();
-    const len = $('INPUT#language_code').val();
+const $ = window.$;
+$(function () {
+  const url = 'https://www.fourtonfish.com/hellosalut/hello/';
+  $('input#btn_translate').click(function () {
+    const lang = $('input#language_code').val();
+    $('div#hello').text(lang);
     $.ajax({
-      type: 'GET',
-      url: 'https://www.fourtonfish.com/hellosalut/hello/' + len,
-      success: function (data) {
-        $('DIV#hello').append(data.hello);
+      url: url,
+      data: { lang: lang },
+      success: function (response) {
+        $('div#hello').text(response.hello);
       }
     });
-  }
-  $('INPUT#btn_translate').click(function () {
-    translate();
   });
-  $('INPUT#language_code').keypress(function (e) {
-    const key = e.which;
-    if (key === 13) {
-      translate();
+  $('input#language_code').keypress(function (e) {
+    if (e.which === 13) {
+      $('input#btn_translate').click();
     }
   });
 });
